955eb1855b813ccb5fc85b9c4db10d6ba77ead3c
Harry Marr <harry.marr@gmail.com>
2011-12-22 15:06:41 +0000

    Upgrade from robbyrussell as well

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,6 +1,7 @@
 current_path=`pwd`
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 ( cd $ZSH && git pull origin master )
+( cd $ZSH && git pull robbyrussell master )
 printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
 printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
 printf '\033[0;32m%s\033[0m\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
